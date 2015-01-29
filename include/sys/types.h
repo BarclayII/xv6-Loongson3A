570e5254a754b1d6603cc74e4a9b454299e73bc6
BarclayII <coin2028@hotmail.com>
2015-01-29 14:51:17 +0800

    added type definitions

---
+++ include/sys/types.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2015 Gan Quan <coin2028@hotmail.com>
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ *
+ */
+
+#ifndef _SYS_TYPES_H
+#define _SYS_TYPES_H
+
+typedef unsigned char uint8, uchar, __u8;
+typedef signed char __s8;
+typedef unsigned short uint16, ushort, __u16;
+typedef signed short __s16;
+typedef unsigned int uint32, uint, __u32;
+typedef signed int __s32;
+typedef unsigned long long uint64, __u64;
+typedef signed long long int64, __s64;
+
+#endif
