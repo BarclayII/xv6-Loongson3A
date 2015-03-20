292130741b4aeff508cc6f3c8debd1b6489964eb
BarclayII <coin2028@hotmail.com>
2015-03-21 00:55:26 +0800

    uvm-test commit #3

--- arch/mips/include/asm/mm/hier/vmm.h
+++ arch/mips/include/asm/mm/hier/vmm.h
@@ -39,39 +39,13 @@ struct pagedesc {
 	unsigned short	ptx;
 };
 
+struct page;
+
 void arch_mm_init(void);
 void dump_pagedesc(addr_t vaddr, struct pagedesc *pdesc);
-
-static inline void new_arch_mm(arch_mm_t *arch_mm)
-{
-}
-
-static inline void destroy_arch_mm(arch_mm_t *arch_mm)
-{
-}
-
-static inline int arch_map_page(arch_mm_t *arch_mm, addr_t vaddr,
-    struct page *p, unsigned int perm)
-{
-	int retcode;
-	struct page *replace = NULL;
-	retcode = pgtable_insert(&(arch_mm->pgd), vaddr, p, perm, true,
-	    &replace);
-	if (retcode != 0)
-		return retcode;
-	if (replace != NULL)
-		pgfree(replace);
-	return 0;
-}
-
-static inline struct page *arch_unmap_page(arch_mm_t *arch_mm, addr_t vaddr)
-{
-	return pgtable_remove(&(arch_mm->pgd), vaddr);
-}
-
-static inline unsigned int page_perm(unsigned long vm_flags)
-{
-	return (vm_flags & VMA_WRITE) ? PTE_VALID|PTE_DIRTY : PTE_VALID;
-}
+int arch_map_page(arch_mm_t *arch_mm, addr_t vaddr, struct page *p,
+    unsigned int perm);
+int new_arch_mm(arch_mm_t *arch_mm);
+void destroy_arch_mm(arch_mm_t *arch_mm);
 
 #endif
